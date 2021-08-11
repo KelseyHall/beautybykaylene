d5e53d9606b8b77fb0267f61c5729ed0c929ae14
Kelsey H <kels-hall23@hotmail.com>
2021-08-11 11:16:09 -0400

    fixed services page

---
+++ src/pages/hard-typed-services-reference.js
@@ -0,0 +1,119 @@
+{
+  /*<div className="services-content ">
+        <div className="services-about-content  service-left-col full-bleed-left">
+          <h2 className="h2-title primary">about the service</h2>
+          <img
+            src={Placeholder}
+            className="services-Img"
+            alt="service-theme-photo"
+          />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consectetur
+            pulvinar viverra eu sem maecenas leo. Ut donec commodo posuere ac
+            at. Massa adipiscing ac enim eget faucibus justo, ut. Felis luctus
+            arcu, duis purus. Eget turpis sit proin ullamcorper. Ut morbi neque
+            at sed neque viverra sit dui. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Consectetur pulvinar viverra eu sem
+            maecenas leo. Ut donec commodo posuere ac at. Massa adipiscing ac
+            enim eget faucibus justo, ut. Felis luctus arcu, duis purus.{' '}
+          </p>
+  </div>*/
+}
+
+{
+  /*<div className="services-pricing service-right-col full-bleed-right">
+          <h2 className="h2-title primary">pricing</h2>
+          <ul className="services-item">
+            <li>
+              <p>item name</p>
+              <p className="price">$99.99</p>
+            </li>
+            <li>
+              <p>item name</p>
+              <p className="price">$99.99</p>
+            </li>
+            <li>
+              <p>item name</p>
+              <p className="price">$99.99</p>
+            </li>
+            <li>
+              <p>item name</p>
+              <p className="price">$99.99</p>
+            </li>
+            <li>
+              <p>item name</p>
+              <p className="price">$99.99</p>
+            </li>
+          </ul>
+
+          <ul className="services-item">
+            <li>
+              <p>item name</p>
+              <p className="price">$99.99</p>
+            </li>
+            <li>
+              <p>item name</p>
+              <p className="price">$99.99</p>
+            </li>
+            <li>
+              <p>item name</p>
+              <p className="price">$99.99</p>
+            </li>
+            <li>
+              <p>item name</p>
+              <p className="price">$99.99</p>
+            </li>
+            <li>
+              <p>item name</p>
+              <p className="price">$99.99</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="services-content">
+        <div className="services-pricing service-left-col-2 full-bleed-left">
+          <h2 className="h2-title primary">pricing</h2>
+          <ul className="services-item">
+            <li>
+              <p>item name</p>
+              <p className="price">$99.99</p>
+            </li>
+            <li>
+              <p>item name</p>
+              <p className="price">$99.99</p>
+            </li>
+            <li>
+              <p>item name</p>
+              <p className="price">$99.99</p>
+            </li>
+            <li>
+              <p>item name</p>
+              <p className="price">$99.99</p>
+            </li>
+            <li>
+              <p>item name</p>
+              <p className="price">$99.99</p>
+            </li>
+          </ul>
+</div>*/
+}
+{
+  /*<div className="services-about-content service-right-col-2 full-bleed-right">
+          <h2 className="h2-title primary">about the service</h2>
+          <img
+            src={Placeholder}
+            className="services-Img"
+            alt="services-theme-photo"
+          />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Consectetur
+            pulvinar viverra eu sem maecenas leo. Ut donec commodo posuere ac
+            at. Massa adipiscing ac enim eget faucibus justo, ut. Felis luctus
+            arcu, duis purus. Eget turpis sit proin ullamcorper. Ut morbi neque
+            at sed neque viverra sit dui. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Consectetur pulvinar viverra eu sem
+            maecenas leo. Ut donec commodo posuere ac at. Massa adipiscing ac
+            enim eget faucibus justo, ut. Felis luctus arcu, duis purus.{' '}
+          </p>
+        </div>*/
+}
