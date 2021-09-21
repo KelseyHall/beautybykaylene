2bc327c40c7a9e876dd9a73dc0cadafd305c4fc3
Kelsey H <kels-hall23@hotmail.com>
2021-09-21 10:12:53 -0400

    working on filter

--- src/components/FaqPage/Questions.js
+++ src/components/FaqPage/Questions.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { FaqQuestions } from '../Staticdatabase';
 
 export const filteredQuestions = (option) => {
   console.log(option);
+};
+const FilteredQuestions = (e) => {
+  const [category, setCategory] = useState([FaqQuestions]);
+  const FilterByCategory = (e) => setCategory((category) => category.filter(e));
+  return (
+    <div key={1} className="QA">
+      <p className="question-bold secondary">{}</p>
+      <p>{}</p>
+    </div>
+  );
 };
 
 const DisplayQuestions = () =>
--- src/components/Staticdatabase.js
+++ src/components/Staticdatabase.js
@@ -269,7 +269,7 @@ export const GalleryDatabase = [
 export const FaqQuestions = [
   {
     id: uuidv4(),
-    type: 'nails',
+    category: 'nails',
     question:
       'nails Lorem ipsum dolor sit amet, ipsum dolor sit amet consectetur adipiscing eli?',
     answer:
@@ -277,14 +277,14 @@ export const FaqQuestions = [
   },
   {
     id: uuidv4(),
-    type: 'waxing',
+    category: 'waxing',
     question: 'waxing Lorem ipsum dolor sit amet, consectetur adipiscing eli?',
     answer:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt inneque, sed quis arcu turpis gravida aliquam dignissim. Bibendum netussed amet id eleifend bibendum convallis. Dui odio purus, laoreet felismorbi nisi, tellus vitae. Ultricies urna porta est in tellus consecteturfaucibus convallis dictum. Risus hendrerit maecenas nec non cras egetelementum. Tellus phasellus nulla urna morbi. In tincidunt elit nullamaecenas. Neque, aliquam id aenean risus eget metus lectus felis in. Eudictum eu, ac quis sit non. Cursus sagittis et ullamcorper non pharetra,fringilla mauris. Ut et enim proin lectus purus. Cursus vulputate atfaucibus varius purus vitae fermentum. Lorem ullamcorper vitae volutpatsit consectetur faucibus suspendisse tortor integer. Rutrum venenatisquam leo in at vitae turpis pretium vitae. Pellentesque feugiat arcucras at. Nibh non pharetra donec egestas euismod purus vestibulum. Namfringilla dui varius nec ac morbi sollicitudin sit fermentum. Et congue',
   },
   {
     id: uuidv4(),
-    type: 'lashbrow',
+    category: 'lashbrow',
     question:
       'lashbrow Lorem ipsum dolor sit amet, ipsum dolor sit amet consectetur adipiscing eli?',
     answer:
@@ -292,7 +292,7 @@ export const FaqQuestions = [
   },
   {
     id: uuidv4(),
-    type: 'massage',
+    category: 'massage',
     question:
       'massage Lorem ipsum dolor sit amet, ipsum dolor sit amet consectetur adipiscing eli?',
     answer:
@@ -300,7 +300,7 @@ export const FaqQuestions = [
   },
   {
     id: uuidv4(),
-    type: 'general',
+    category: 'general',
     question:
       'general Lorem ipsum dolor sit amet, ipsum dolor sit amet consectetur adipiscing eli?',
     answer:
