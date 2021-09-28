71c6d1e829aa785b0dcad82534ecd5d64d93bd06
Kelsey H <kels-hall23@hotmail.com>
2021-09-28 16:50:54 -0400

    worked on footer

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,16 +1,17 @@
 import React from 'react';
-
+import SocialMedia from './SocialMedia';
 const ContactInfo = () => {
   return (
     <div className="contact-info-footer">
       <div className="info-wrapper">
-        <p className="primary">text</p>
+        <p className="primary">call or text</p>
         <p className="secondary">905.644.8880</p>
       </div>
       <div className="info-wrapper">
-        <p className="primary">email</p>
+        <p className="primary">send us an email at</p>
         <p className="secondary">beautybykaylene@gmail.com</p>
       </div>
+      <SocialMedia />
     </div>
   );
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,6 @@ import React from 'react';
 import SimpleLogo from '../images/simpleKayleneTitle.png';
 import ContactInfo from './ContactInfo';
 
-import SocialMedia from './SocialMedia';
-
 const Footer = () => {
   return (
     <div>
@@ -12,9 +10,8 @@ const Footer = () => {
         <img src={SimpleLogo} alt="Beauty by Kaylene" className="logo" />
         <ContactInfo />
       </div>
-      <p>
-        <SocialMedia />© copyright 2021 |{' '}
-        <a href="#privacyPolicy">privacy policy</a>
+      <p className="copyright">
+        © copyright 2021 | <a href="#privacyPolicy">privacy policy</a>
       </p>
     </div>
   );
