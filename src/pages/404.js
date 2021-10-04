388e343d5ea860f9ee548e9cd1eb49c25045b076
Kelsey H <kels-hall23@hotmail.com>
2021-10-04 13:05:29 -0400

    designed 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import notFoundCartoon from '../images/gossmani.png';
 
 const PageNotFound = () => {
   return (
-    <div>
-      <p>Page not found</p>
+    <div className="pageNotFound">
+      <h1 className="h1-title primary">Oops something went wrong</h1>
+      <div className="pageNotFound-contents">
+        <img
+          src={notFoundCartoon}
+          alt="Gossamers getting a manicure from bugs bunny"
+        />
+        <h3 className="h3-title secondary">lets get you back on track</h3>
+        <a href="/" className="button">
+          bring me home
+        </a>
+      </div>
     </div>
   );
 };
